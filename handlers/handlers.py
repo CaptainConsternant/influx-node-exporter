f1b9a26b8e93194b3b934b51c52b63aee9a43ff9
alex <alexandre@garaia.fr>
2022-12-09 10:50:35 +0100

    first sensors

---
+++ handlers/handlers.py
@@ -0,0 +1,24 @@
+from decouple import config
+from influxdb import InfluxDBClient
+from loguru import logger as log
+
+
+class Handler():
+    database_name = "default"
+
+    
+    def __init__(self, *args, **kwargs):
+        # TODO implement >>> client = InfluxDBClient(host='mydomain.com', port=8086, username='myuser', password='mypass' ssl=True, verify_ssl=True)
+        self.client = InfluxDBClient(host=config("INFLUXDB_HOST",default='localhost'), port=config("INFLUXDB_PORT", default=8086))
+        self.client.create_database(self.database_name)
+        self.client.switch_database(self.database_name)
+        log.info(f"Successfully connected to {self.database_name}")
+
+        self.data_buffer=[]
+
+    def collect(self):
+        raise NotImplementedError
+
+    def write_buffer_to_db(self):
+        self.client.write_points(self.data_buffer)
+        self.data_buffer=[]
---
+++ handlers/sensors.py
@@ -0,0 +1,56 @@
+import datetime
+import json
+import re
+from subprocess import check_output
+
+from .handlers import Handler
+
+
+class SensorsHandler(Handler):
+    """
+    Collects information based on lm_sensor
+
+    :param Handler: _description_
+    :type Handler: _type_
+    """    
+    database_name = "sensors"
+
+    def collect(self):
+        raw = json.loads(check_output(['sensors','-j']).decode())
+        for k,vals in raw.items():
+            if "coretemp" in k :
+
+                for coreidstr, fields in vals.items():
+                    if isinstance(fields,dict):
+                        isa_num=(re.findall('\d', k) or [''])[0]
+                        core_num=(re.findall('\d', coreidstr) or [''])[0]
+                        identifier =f"{isa_num}-{core_num}"
+                        clean_fields = {re.sub('\d','',y):t for y,t in fields.items()}
+                        r= {
+                                "measurement": "cpu_temp",
+                                "tags":{
+                                    'identifier': identifier,
+                                },
+                                "fields":clean_fields,
+                                "time":datetime.datetime.now().isoformat()
+
+                            }
+                        self.data_buffer.append(r)
+            elif "power" in k:
+                for pduidstr, fields in vals.items():
+                    if isinstance(fields,dict):
+                        sens_num=(re.findall('\d', k) or [''])[0]
+                        pdu_num=(re.findall('\d', pduidstr) or [''])[0]
+                        identifier =f"{pdu_num}-{core_num}"
+                        clean_fields = {re.sub('\d','',y):t for y,t in fields.items()}
+                        r= {
+                                "measurement": "pow_meter",
+                                "tags":{
+                                    'identifier': identifier,
+                                },
+                                "fields":clean_fields,
+                                "time":datetime.datetime.now().isoformat()
+
+                            }
+                        self.data_buffer.append(r)
+ 
